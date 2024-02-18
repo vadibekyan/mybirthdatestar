89c5bd8f7fdda9ebc927c19c13f96a2bc3305d66
vadibekyan <adbvardan@gmail.com>
2024-02-18 17:07:27 +0000

    Replace starsforall info with myBDstar

--- src/_data/env.js
+++ src/_data/env.js
@@ -1,7 +1,7 @@
 const environment = process.env.ELEVENTY_ENV;
 const DEV_ENV = 'dev';
 const devUrl = 'http://localhost:8080';
-const prodUrl = 'https://starsforall.com';
+const prodUrl = 'https://mybirthdatestar.com';
 const isProd = environment !== DEV_ENV;
 const url = isProd ? prodUrl : devUrl;
 
@@ -10,7 +10,7 @@ module.exports = {
     isProd,
     url,
     tracking: {
-        gtag: 'G-N74L44QKWL',
-        pixelId: 'MY-PIXEL-ID',
+        gtag: 'G-',
+        pixelId: 'MY',
     },
 };
